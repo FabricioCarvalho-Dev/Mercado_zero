5250b181b2b431c3aa85b5c74d9118a0c8cdc0e4
Fabricio Carvalho <fabriciosouzza99@gmail.com>
2024-08-15 14:12:35 -0300

    mais alguns ajustes no site, criando a tela de taxa

--- src/main/java/fabriciodev/bean/DashboardBean.java
+++ src/main/java/fabriciodev/bean/DashboardBean.java
@@ -1,13 +1,17 @@
 package fabriciodev.bean;
 
 import fabriciodev.dao.UsuarioDao;
+import fabriciodev.model.Produto;
 import fabriciodev.model.Usuario;
+import org.primefaces.PrimeFaces;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.application.FacesMessage;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @ManagedBean
 @SessionScoped
@@ -16,11 +20,58 @@ public class DashboardBean implements Serializable {
     private UsuarioDao usuarioDao;
     private Usuario usuario;
     private static final String TELA_PRODUTOS = "produtos";
-    
+    private int quantidade;
+    private double precoTotal;
+    private double porcentagem;
+    private double precoPorUnidade;
+    private double precoComPorcentagem;
+
     // Inicializa o bean
     public DashboardBean() {
         usuarioDao = new UsuarioDao();
+        precoPorUnidade = 0.0;
+        precoComPorcentagem = 0.0;
+        quantidade = 0;
+        precoTotal = 0.0;
+        porcentagem = 0.0;
         // Inicialização adicional se necessário
+    }
+    public void abrirModal() {
+        abrirDialogoTaxa();
+    }
+
+    // Método para calcular o preço por unidade e o preço com porcentagem
+    public void calcularPrecos() {
+        if (quantidade > 0) {
+            precoPorUnidade = precoTotal / quantidade;
+            precoComPorcentagem = precoPorUnidade * (1 + porcentagem / 100);
+        } else {
+            precoPorUnidade = 0.0;
+            precoComPorcentagem = 0.0;
+        }
+        limpaCampos();
+    }
+    public void limpaCampos(){
+        quantidade = 0;
+        precoTotal = 0.0;
+    }
+    public String getPrecoComPorcentagemLabel() {
+        return String.format("Preço com %.00f%% de Porcentagem R$: %.2f", porcentagem, precoComPorcentagem);
+    }
+    public void abrirDialogoTaxa() {
+        Map<String, Object> opcoes = new HashMap<>();
+        opcoes.put("modal", true);
+        opcoes.put("resizable", false);
+        opcoes.put("contentHeight", 470);
+
+        PrimeFaces.current().dialog().openDynamic("modalCalcularTaxaProduto", opcoes, null);
+    }
+    private void openDialog() {
+        PrimeFaces.current().executeScript("PF('dialogTaxaProduto').show();");
+    }
+
+    private void closeDialog() {
+        PrimeFaces.current().executeScript("PF('dialogTaxaProduto').hide();");
     }
     
     // Métodos de ação
@@ -52,5 +103,36 @@ public class DashboardBean implements Serializable {
     }
     
     // Getters e Setters para propriedades necessárias
+    // Getters e Setters
+    public int getQuantidade() {
+        return quantidade;
+    }
 
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoTotal() {
+        return precoTotal;
+    }
+
+    public void setPrecoTotal(double precoTotal) {
+        this.precoTotal = precoTotal;
+    }
+
+    public double getPorcentagem() {
+        return porcentagem;
+    }
+
+    public void setPorcentagem(double porcentagem) {
+        this.porcentagem = porcentagem;
+    }
+
+    public double getPrecoPorUnidade() {
+        return precoPorUnidade;
+    }
+
+    public double getPrecoComPorcentagem() {
+        return precoComPorcentagem;
+    }
 }
